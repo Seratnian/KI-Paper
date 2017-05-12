6b89313704cbeefaa6e3eaa6ad13aa4a40688c6e
Seratnian <sschleier@posteo.de>
2017-05-12 13:12:28 +0200

    TrackValues erweitert
    liefert jetzt .csv-Format
    Updates optional in fixen Abständen
    Schreiben über bufferedFileWriter
    Distance-Tracking zu Mauern möglich

--- Assets/Scripts/trackValues.cs
+++ Assets/Scripts/trackValues.cs
@@ -1,41 +1,40 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using UnityEngine;
-using UnityEngine.AI;
 
-public class trackValues : MonoBehaviour
+public class TrackValues : MonoBehaviour
 {
-    public bool writeToFile = false;
-    private System.DateTime now = System.DateTime.Now;
+    public bool debug = false;
+    public bool writeToFile = true;
+    private string logFile;
+    private FileStream logFileStream;
+    public bool trackContinuously = true;
+    private DateTime now = DateTime.Now;
+    public static readonly string CSV_SEPARATOR = ",";
+
+    private bool trackPosition = false;
+    private float everyXSeconds = 1;
+    public int discreteAreaInto = 100;
+    public GameObject area = null;
+    private Vector3 sizeOfGround;
+    private float timeOfLastPositionTrack = 0;
 
     public bool trackSplitTimes = false;
     public Transform[] whereToTrack = new Transform[5];
     private readonly float DISTANCE_TO_TARGET = 3;
     private readonly int TIME_BETWEEN_TRACKS = 5;
     private float timeOfLastTimeTrack = 0;
+    
+    public bool trackCollisions = true;
+    private string collisionName = "";
 
-    /*
-    public bool detectSwinging = false;
-    public MeshRenderer ground = null;
-    private int lastAreaID;
-    */
-
-    public bool trackCollisions = false;
-
-    public bool trackPosition = false;
-    public float everyXSeconds = 1;
-    public int discreteAreaInto = 100;
-    public GameObject area = null;
-    private Vector3 sizeOfGround;
-    private float timeOfLastPositionTrack = 0;
-
-    public bool trackDistanceToObject = false;
-    public GameObject[] distanceTo = new GameObject[1];
-
-    public float everyYSeconds = 1;
-    public bool fromCornerToCorner = false;
+    public bool trackDistanceToObject = true;
+    public GameObject walls = null;
+    private GameObject[] distanceTo = new GameObject[1];
+    private float everyYSeconds = 1;
+    public bool fromCornerToCorner = true;
     private float timeOfLastDistanceTrack = 0;
 
 
@@ -43,82 +42,196 @@ public class trackValues : MonoBehaviour
     void Start ()
     {
         sizeOfGround = area.GetComponent<Renderer>().bounds.size;
+        if (trackContinuously)
+        {
+            if (writeToFile)
+            {
+                logFile = Application.dataPath.Replace("Assets", "") + "Logs/" + "log_" + now.ToString("yyyy'-'MM'-'dd HH'_'mm") + ".csv";
+                logFileStream = File.Create(logFile);
+                Write("sep=" + CSV_SEPARATOR);
+                List<string> titles = new List<string>();
+                titles.Add("Time");
+                titles.Add("x-Position");
+                titles.Add("y-Position");
+                if (trackSplitTimes) titles.Add("SplitTimes");
+                if (trackDistanceToObject) titles.Add("Distance");
+                if (trackCollisions) titles.Add("Collision");
+                Write(separate(titles.ToArray()));
+            }
+            if (trackDistanceToObject)
+            {
+                foreach (Transform wall in walls.transform)
+                {
+                    wall.name = positionToName(calculatePosition(wall));
+                }
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (writeToFile) logFileStream.Close();
+    }
+
+    private void Write(string text)
+    {
+        Byte[] textBytes = new UTF8Encoding(true).GetBytes(text + Environment.NewLine);
+        logFileStream.Write(textBytes, 0, textBytes.Length);
+    }
+
+    private void FixedUpdate()
+    {
+        if (trackContinuously)
+        {
+            //float distance = float.MaxValue, string collision = null, string splitTime = null
+            Vector2 position = calculatePosition(transform);
+            Tracker tracker = new Tracker(Time.time, position, debug);
+            if (trackSplitTimes) tracker.SplitTime = checkSplitTime();
+            if (trackCollisions) tracker.Collision = checkCollision();
+            if (trackDistanceToObject) tracker.Distance = calculateDistance(position);
+            
+            log(tracker.ToString());
+        }
+    }
+
+    private string checkCollision()
+    {
+        string collision = collisionName;
+        collisionName = "";
+        return collision;
+    }
+
+    private string checkSplitTime()
+    {
+        foreach (Transform splitTimeTracker in whereToTrack)
+        {
+            if (Vector3.Distance(this.transform.position, splitTimeTracker.position) <= DISTANCE_TO_TARGET)
+            {
+                return splitTimeTracker.name;
+            }
+        }
+        return "";
+    }
+
+    private float calculateDistance(Vector2 position, int discreteDistance = 1)
+    {
+        List<string> wallNames = new List<string>();
+        if (discreteDistance == 1) wallNames.Add(positionToName(position));
+        for (int x = -discreteDistance; x <= discreteDistance; x++)
+        {
+            for (int y = -discreteDistance; y <= discreteDistance; y++)
+            {
+                if (Math.Abs(x) == discreteDistance || Math.Abs(y) == discreteDistance)
+                {
+                    int finalX = (int)position.x + x;
+                    int finalY = (int)position.y + y;
+                    if (finalX >= 0 && finalX < discreteAreaInto && finalY >= 0 && finalY < discreteAreaInto)
+                        wallNames.Add(positionToName(finalX, finalY));
+                }
+            }
+        }
+
+        List<float> distances = new List<float>();
+        foreach(Transform wall in walls.transform)
+        {
+            if (wallNames.Exists(x => x == wall.name))
+            {
+                distances.Add(calculateDistance(gameObject, wall.gameObject).magnitude);
+            }
+        }
+        distances.Sort();
+        if (distances.Count > 0) return distances[0];
+        return calculateDistance(position, discreteDistance + 1);
+    }
+
+    private string positionToName(int x, int y)
+    {
+        return "Wall_" + x.ToString().PadLeft(2, '0') + "_" + y.ToString().PadLeft(2, '0');
+    }
+
+    private string positionToName(Vector2 position)
+    {
+        return positionToName((int) position.x, (int) position.y);
+    }
+
+    private Vector2 calculatePosition(Transform of)
+    {
+        Vector2 position = new Vector2();
+        position.x = (int)Math.Floor(of.position.x / sizeOfGround.x * discreteAreaInto) + discreteAreaInto / 2;
+        position.y = (int)Math.Floor(of.position.z / sizeOfGround.z * discreteAreaInto) + discreteAreaInto / 2;
+        return position;
     }
 
     // Update is called once per frame
-    void Update () {
-        if (trackSplitTimes)
+    void Update()
+    {
+        if (!trackContinuously)
         {
-            if (Time.time - timeOfLastTimeTrack >= TIME_BETWEEN_TRACKS)
+            if (trackSplitTimes)
             {
-                foreach (Transform splitTimeTracker in whereToTrack)
+                if (Time.time - timeOfLastTimeTrack >= TIME_BETWEEN_TRACKS)
                 {
-                    if (Vector3.Distance(this.transform.position, splitTimeTracker.position) <= DISTANCE_TO_TARGET)
+                    foreach (Transform splitTimeTracker in whereToTrack)
                     {
-                        timeOfLastTimeTrack = Time.time;
-                        log("split time", splitTimeTracker.name, + timeOfLastTimeTrack);
+                        if (Vector3.Distance(this.transform.position, splitTimeTracker.position) <= DISTANCE_TO_TARGET)
+                        {
+                            timeOfLastTimeTrack = Time.time;
+                            log("split time", splitTimeTracker.name, +timeOfLastTimeTrack);
+                        }
                     }
                 }
             }
-        }
-        /*
-        if (detectSwinging)
-        {
-            string path = Application.dataPath + "\\Scenes\\maze\\NavMesh.asset";
-            log(path);
-            Mesh navMesh = (Mesh) Resources.Load(path);
-            log(navMesh);
-
-            string[] allPaths = Directory.GetFiles(Application.dataPath, "*", SearchOption.AllDirectories);
-            foreach (string path2 in allPaths)
+            if (trackPosition)
             {
-                log(path2);
-                log(path);
-                //Object mesh = Asset
-                //print(mesh);
-            }
-        }
-        */
-        if (trackPosition)
-        {
-            if (Time.time - timeOfLastPositionTrack >= everyXSeconds)
-            {
-                timeOfLastPositionTrack = Time.time;
-                int x = (int)Math.Floor(transform.position.x / sizeOfGround.x * discreteAreaInto) + discreteAreaInto/2;
-                int z = (int)Math.Floor(transform.position.z / sizeOfGround.z * discreteAreaInto) + discreteAreaInto/2;
-                log("position", x, z);
-            }
-        }
-
-        if (trackDistanceToObject)
-        {
-            if (Time.time - timeOfLastDistanceTrack >= everyYSeconds)
-            {
-                timeOfLastDistanceTrack = Time.time;
-                foreach (GameObject target in distanceTo)
+                if (Time.time - timeOfLastPositionTrack >= everyXSeconds)
                 {
-                    Vector3 thisPosition = transform.position;
-                    Vector3 targetPosition;
-                    if (fromCornerToCorner)
-                    {
-                        targetPosition = getClosestPoint(target, thisPosition);
-                        thisPosition = getClosestPoint(gameObject, targetPosition);
-                    }
-                    else
-                    {
-                        targetPosition = target.transform.position;
-                    }
-                    log("distance", (thisPosition.x - targetPosition.x), (thisPosition.y - targetPosition.y), (thisPosition.z - targetPosition.z));
+                    timeOfLastPositionTrack = Time.time;
+                    Vector2 position = calculatePosition(transform);
+                    log("position", position.x, position.y);
                 }
             }
+
+            if (trackDistanceToObject)
+            {
+                if (Time.time - timeOfLastDistanceTrack >= everyYSeconds)
+                {
+                    timeOfLastDistanceTrack = Time.time;
+                    foreach (GameObject target in distanceTo)
+                    {
+                        Vector3 distance = calculateDistance(gameObject, target);
+                        log("distance", distance.x, distance.y, distance.z);
+                    }
+                }
+            }
+            if (trackCollisions && collisionName != "")
+            {
+                log("Collision", collisionName);
+                collisionName = "";
+            }
         }
+    }
+
+    private Vector3 calculateDistance(GameObject gameObject, GameObject target)
+    {
+        Vector3 thisPosition = transform.position;
+        Vector3 targetPosition;
+        if (fromCornerToCorner)
+        {
+            targetPosition = calculateClosestPoint(target, thisPosition);
+            thisPosition = calculateClosestPoint(gameObject, targetPosition);
+        }
+        else
+        {
+            targetPosition = target.transform.position;
+        }
+        return new Vector3((thisPosition.x - targetPosition.x), (thisPosition.y - targetPosition.y), (thisPosition.z - targetPosition.z));
     }
 
     void OnCollisionEnter(Collision collision)
     {
         if (trackCollisions)
         {
-            log("collision", collision.collider.name);
+            collisionName = collision.collider.name;
         }
     }
 
@@ -131,19 +244,14 @@ public class trackValues : MonoBehaviour
         {
             if (o != null)
             {
-                text += o.ToString().PadRight(16);
+                text += o.ToString() + ",";
             }
         }
-        text += Environment.NewLine;
+        text = text.Substring(0, text.Length - 1);
 
         if (writeToFile)
         {
-            string directoryPath = Application.dataPath.Replace("Assets", "") + "Logs/";
-            if (!Directory.Exists(directoryPath))
-            {
-                Directory.CreateDirectory(directoryPath);
-            }
-            System.IO.File.AppendAllText(directoryPath + "log_" + now.ToString("yyyy'-'MM'-'dd HH'_'mm") + ".txt", text);
+            Write(text);
         }
         else
         {
@@ -151,7 +259,7 @@ public class trackValues : MonoBehaviour
         }
     }
 
-    private Vector3 getClosestPoint(GameObject from, Vector3 to)
+    private Vector3 calculateClosestPoint(GameObject from, Vector3 to)
     {
         Vector3 fromPosition;
         if (from.GetComponent<Collider>() != null)
@@ -167,5 +275,80 @@ public class trackValues : MonoBehaviour
             fromPosition = from.transform.position;
         }
         return fromPosition;
+    }
+    public static string separate(string string1, string string2 = null, string string3 = null, string string4 = null, string string5 = null, string string6 = null, string string7 = null, string string8 = null)
+    {
+        string[] strings = { string1, string2, string3, string4, string5, string6, string7, string8 };
+        return separate(strings);
+    }
+    public static string separate(string[] strings)
+    {
+        string textToReturn = "";
+        foreach (string text in strings)
+        {
+            if (text == null)
+            {
+                break;
+            }
+            textToReturn += text + CSV_SEPARATOR;
+        }
+        return textToReturn.Substring(0, textToReturn.Length - 1);
+    }
+}
+
+class Tracker
+{
+    private float time;
+    private Vector2 position;
+    private bool debug;
+    private float distance;
+    private string collision;
+    private string splitTime;
+
+    public Tracker(float time, Vector2 position, bool debug, float distance = float.MaxValue, string collision = null, string splitTime = null)
+    {
+        this.time = time;
+        this.position = position;
+        this.debug = debug;
+        this.distance = distance;
+        this.collision = collision;
+        this.splitTime = splitTime;
+    }
+    public Tracker(float time, Vector2 position, float distance = float.MaxValue, string collision = null, string splitTime = null)
+    : this(time, position, false, distance, collision, splitTime) { }
+
+    public float Distance
+    {
+        set
+        {
+            distance = value;
+        }
+    }
+
+    public string Collision
+    {
+        set
+        {
+            collision = value;
+        }
+    }
+
+    public string SplitTime
+    {
+        set
+        {
+            splitTime = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        List<string> strings = new List<string>();
+        strings.Add((debug ? "Time: " : "") + time);
+        strings.Add((debug ? "Position: " : "") + TrackValues.separate(position.x.ToString(), position.y.ToString()));
+        if (distance < float.MaxValue) strings.Add((debug ? "Distance: " : "") + distance);
+        if (collision != null) strings.Add((debug ? "Collision: " : "") + collision);
+        if (splitTime != null) strings.Add((debug ? "splitTime: " : "") + splitTime);
+        return TrackValues.separate(strings.ToArray());
     }
 }
